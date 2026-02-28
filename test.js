02d460e51454d93032f3edc44d9b5e8c4f67fa15
pongpoti <pong.poti@gmail.com>
2026-02-28 09:39:56 +0000

    c

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js"
 const supabase = createClient("https://akexkcjzgnlqlqrfxtdi.supabase.co", "sb_publishable_f9sKJUhh5ppG4tIyAXvF6A_hArSH2Af")
 
 const main = async () => {
-  const res_check_id_null = await supabase.from("data").select().eq("id", "111")
-   console.log(res_check_id_null.data.length)
+  const { data } = await supabase.from("users").select("*")
+  console.log(data)
 }
 
 main()
